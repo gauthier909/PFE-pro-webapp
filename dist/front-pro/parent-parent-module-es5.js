930d0954a549b73cf34a1cd359a7723fe8efdfcf
Niklas GROSSE <niklas.grosse@student.vinci.be>
2019-12-09 17:49:09 +0100

    master to staging

---
+++ dist/front-pro/parent-parent-module-es5.js
@@ -0,0 +1,36 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["parent-parent-module"], {
+        /***/ "./src/app/parent/parent.module.ts": 
+        /*!*****************************************!*\
+          !*** ./src/app/parent/parent.module.ts ***!
+          \*****************************************/
+        /*! exports provided: ParentModule */
+        /***/ (function (module, __webpack_exports__, __webpack_require__) {
+            "use strict";
+            __webpack_require__.r(__webpack_exports__);
+            /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ParentModule", function () { return ParentModule; });
+            /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+            /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+            /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm2015/common.js");
+            /* harmony import */ var _parent_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./parent-routing.module */ "./src/app/parent/parent-routing.module.ts");
+            /* harmony import */ var _parent_parent_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./parent/parent.component */ "./src/app/parent/parent/parent.component.ts");
+            /* harmony import */ var _hello_world_hello_world_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../hello-world/hello-world.component */ "./src/app/hello-world/hello-world.component.ts");
+            var ParentModule = /** @class */ (function () {
+                function ParentModule() {
+                }
+                return ParentModule;
+            }());
+            ParentModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+                Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+                    declarations: [_parent_parent_component__WEBPACK_IMPORTED_MODULE_4__["ParentComponent"], _hello_world_hello_world_component__WEBPACK_IMPORTED_MODULE_5__["HelloWorldComponent"]],
+                    imports: [
+                        _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                        _parent_routing_module__WEBPACK_IMPORTED_MODULE_3__["ParentRoutingModule"]
+                    ]
+                })
+            ], ParentModule);
+            /***/ 
+        })
+    }]);
+//# sourceMappingURL=parent-parent-module-es2015.js.map
+//# sourceMappingURL=parent-parent-module-es5.js.map
+//# sourceMappingURL=parent-parent-module-es5.js.map
