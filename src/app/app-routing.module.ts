3c38a59be7249f721013bb7118430157b90668d3
Niklas GROSSE <niklas.grosse@student.vinci.be>
2019-12-10 00:57:43 +0100

    resolve pwa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {UserLoginComponent} from "./user-login/user-login.component";
 import {ChoixEnfantComponent} from "./choix-enfant/choix-enfant.component";
 import {ErrorRoutingComponent} from './error-routing/error-routing.component';
-import {AuthGuard} from './auth/auth.guard';
-import {AuthGuardA} from './auth/adminauth.guard';
-import {AuthGuardP} from './auth/parentauth.guard';
-import {AuthGuardR} from './auth/responssible.guard';
+
 import {EnfantsComponent} from './enfants/enfants.component';
 import {EnfantDetailComponent} from './enfant-detail/enfant-detail.component'
 import {EnfantAjoutComponent} from './enfant-ajout/enfant-ajout.component'
@@ -21,24 +18,6 @@ const routes: Routes = [
     path: '',
     redirectTo: '/auth/login',
     pathMatch: 'full'
-  },  {
-    path: 'responsable',
-    loadChildren: () => import('./responsable/responsable.module').then(mod => mod.ResponsableModule),
-    canLoad: [AuthGuardR]
-  },
-  {
-    path: 'professionel',
-    loadChildren: () => import('./professionelle/professionelle.module').then(mod => mod.ProfessionelleModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'parent',
-    loadChildren: () => import('./parent/parent.module').then(mod => mod.ParentModule),
-    canLoad: [AuthGuardP]
-  },{
-    path:'admin',
-    loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule),
-    canLoad: [AuthGuardA]
   },
 
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import {ProfessionelleModule} from './professionelle/professionelle.module';
-import {AuthRoutingModule} from './auth/auth-routing.module';
+
 
 import { EnfantsComponent } from './enfants/enfants.component';
 import { EnfantDetailComponent } from './enfant-detail/enfant-detail.component';
@@ -20,15 +19,7 @@ import { environment } from '../environments/environment';
 import { GestionProfessionnelComponent } from './gestion-professionnel/gestion-professionnel.component';
 import { PersonneAjoutComponent } from './personne-ajout/personne-ajout.component';
 import { PersonneDetailComponent } from './personne-detail/personne-detail.component';
-import { ResponsableComponent } from './responsable/responsable/responsable.component';
 
-import { ParentComponent } from './parent/parent/parent.component';
-import {ParentRoutingModule} from './parent/parent-routing.module';
-import {ResponsableRoutingModule} from './responsable/responsable-routing.module';
-import {ParentModule} from './parent/parent.module';
-import {ResponsableModule} from './responsable/responsable.module';
-import { AdminComponent } from './admin/admin/admin.component';
-import {AdminModule} from './admin/admin.module';
 import { EnfantRechercheComponent } from './enfant-recherche/enfant-recherche.component';
 import { PersonneRechercheComponent } from './personne-recherche/personne-recherche.component';
 import { FiltresGestionComponent } from './filtres-gestion/filtres-gestion.component';
@@ -48,7 +39,6 @@ import { FiltresGestionComponent } from './filtres-gestion/filtres-gestion.compo
     GestionProfessionnelComponent,
     PersonneAjoutComponent,
     PersonneDetailComponent,
-    ParentComponent,
     EnfantRechercheComponent,
     PersonneRechercheComponent,
     FiltresGestionComponent
@@ -58,11 +48,6 @@ import { FiltresGestionComponent } from './filtres-gestion/filtres-gestion.compo
   imports: [
     BrowserModule,
     HttpClientModule,
-    ResponsableModule,
-    ParentRoutingModule,
-    AdminModule,
-    ProfessionelleModule,
-    AuthRoutingModule,
     AppRoutingModule,
     FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
