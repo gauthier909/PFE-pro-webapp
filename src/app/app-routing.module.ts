d56115f047c9dea1533916dd6a7537651a9bd9ff
Niklas GROSSE <niklas.grosse@student.vinci.be>
2019-12-10 21:22:49 +0100

    config partie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import {GestionProfessionnelComponent} from './gestion-professionnel/gestion-pro
 import {PersonneAjoutComponent} from './personne-ajout/personne-ajout.component'
 import {PersonneDetailComponent} from './personne-detail/personne-detail.component'
 import{FiltresGestionComponent} from './filtres-gestion/filtres-gestion.component'
+import { ConfigurationPartieComponent } from './configuration-partie/configuration-partie.component';
 
 
 const routes: Routes = [
@@ -19,7 +20,10 @@ const routes: Routes = [
     redirectTo: '/auth/login',
     pathMatch: 'full'
   },
-
+  {
+    path:'configuration-partie',
+    component: ConfigurationPartieComponent
+  },
   {
     path: 'choix',
     component: ChoixEnfantComponent
